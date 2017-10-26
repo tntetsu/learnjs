d667b13246cda97af21dfa7d0c98b93c04de346f
Tetsuo TANAKA <t-tanaka@ic.kanagawa-it.ac.jp>
2017-10-26 11:19:43 +0900

    2.3 ルートを表示する

--- public/app.js
+++ public/app.js
@@ -1,7 +1,16 @@
 'use strict';
 var learnjs = {};
 
+learnjs.problemView = () => {
+    return $('<div class="problem-view">').text('Coming soon!');
+}
+
 learnjs.showView = hash => {
-    var problemView = $('<div class="problem-view">').text('Coming soon!');
-    $('.view-container').empty().append(problemView);
+    var routes = {
+        '#problem-1': learnjs.problemView
+    };
+    var viewFn = routes[hash];
+    if (viewFn) {
+        $('.view-container').empty().append(viewFn());
+    }
 }
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,6 +1,10 @@
-describe('learnJS', ()=> {
+describe('LearnJS', ()=> {
     it('can show a problem view', () => {
         learnjs.showView('#problem-1');
         expect($('.view-container .problem-view').length).toEqual(1);
+    });
+    it('shows the landing page view when there is no hash', () =>{
+        learnjs.showView('');
+        expect($('.view-container .landing-view').length).toEqual(1);
     });
 });
